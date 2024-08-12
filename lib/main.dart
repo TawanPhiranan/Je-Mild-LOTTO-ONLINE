ae1459e0a871be3c02c9313ed1fefe729bad4612
TawanPhiranan <65011212119@msu.ac.th>
2024-08-12 17:45:58 +0700

    agin agin

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       // theme: ThemeData(useMaterial3: f),
       title: 'Flutter Demo',
-
+      
     );
   }
 }
