2ecbc12b7bbf27df5064cd35c7b5fb8f3c311113
viewthiti <65011212173@msu.ac.th>
2024-08-27 03:49:08 +0700

    uu

--- lib/config/internal_config.dart
+++ lib/config/internal_config.dart
@@ -1,2 +1,2 @@
-String API_ENDPOINT = "http://172.20.10.3:3000";
-// String API_ENDPOINT = "https://miniprojectlotto.onrender.com";
+// String API_ENDPOINT = "http://172.20.10.3:3000";
+String API_ENDPOINT = "https://miniprojectlotto.onrender.com";
--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
     return  MaterialApp(
       title: 'Flutter Demo',
       home: Loginpage(),
+      
     );
   }
 }
