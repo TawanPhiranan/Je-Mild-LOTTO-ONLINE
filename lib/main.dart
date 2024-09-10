a005aa59b6e6f6c83e093c631244f8e5b0013a30
pimpim_iwi <pimpim_iwi@MacBook-Air-khxng-pimpim-iwi.local>
2024-09-11 03:08:29 +0700

    N

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mini_project/pages/AdminHomePage.dart';
 import 'package:mini_project/pages/AdminLottoDraw.dart';
 import 'package:mini_project/pages/LogoPage.dart';
+import 'package:mini_project/pages/LottoPage.dart';
 import 'package:mini_project/pages/OrderPage.dart';
 
 void main() {
